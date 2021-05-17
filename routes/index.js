c0b206c7c5341d6ae4123ced7337b2895a2b3f75
drizzt00s <drizzt79s@163.com>
2021-05-17 14:25:56 +0800

    d

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  var userData = req.session.userData;
+  var loginInfo = "Sign in";
+  var isDisplayed = "";
+  if(!userData){
+      //用户未登录
+      loginInfo = loginInfo;
+      isDisplayed = "show";
+    }else{
+      //用户已登录
+      loginInfo = userData.account;
+      isDisplayed = "hide";
+    }
+
+
   var connection = utility.createConnection("localhost", "root", "YES", "3306", "app");
   utility.connect(connection);
   connection.query("select * from product_l1", function(err, gpdLists){
@@ -20,7 +35,7 @@ router.get('/', function(req, res, next) {
           throw err;
         }
         console.log(JSON.stringify(d));
-        res.render('index', { title: 'Express',gpdLists:gpdLists,subGpdLists:subGpdLists,lvsubGpdLists3:d});
+        res.render('index', { title: 'Express',gpdLists:gpdLists,subGpdLists:subGpdLists,lvsubGpdLists3:d,loginInfo:loginInfo,isDisplayed:isDisplayed});
       });
 
       
