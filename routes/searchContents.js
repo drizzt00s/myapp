fe362fc776700642ee1da8327381a636246ab4c8
drizzt00s <drizzt79s@163.com>
2021-05-19 10:23:39 +0800

    search

--- routes/searchContents.js
+++ routes/searchContents.js
@@ -8,30 +8,32 @@ router.post("/", function(req, res, next){
     console.log(searchContents);
     var connection = utility.createConnection("localhost", "root", "YES", "3306", "app");
     utility.connect(connection);
-
-
-
-
-
-    connection.query("SELECT * FROM product_l3 LEFT JOIN product_specifications ON product_l3.id = product_specifications.id WHERE des like + " + "'%" + searchContents + "%'" ,function (err,data){
-        if(err){
-            throw err;
-        }
-        console.log(data);
-    });
-    
-    // connection.query("SELECT * FROM product_l3 WHERE des like " + "'%" + searchContents + "%'",function (err,data) {
+    // connection.query("SELECT * FROM product_l3 LEFT JOIN product_specifications ON product_l3.id = product_specifications.id WHERE des like + " + "'%" + searchContents + "%'" ,function (err,data){
     //     if(err){
     //         throw err;
     //     }
+    //     // console.log(data);
     //     var dataLength = 0;
-    //     for(var i=0; i<data.length; i++){
-    //         dataLength++
-    //     }
-    //     console.log(data);
-    //     req.session.searchResult = {searchContents:searchContents,searchResult:data,dataLength:dataLength};
-    //     res.send({code:1});
+    //     req.session.searchResult = {
+    //             searchContents:searchContents,
+    //             searchResult:data,
+    //             dataLength:dataLength,
+    //             spec:spec
+    //     };
     // });
+
+    connection.query("SELECT * FROM product_l3 WHERE des like " + "'%" + searchContents + "%'",function (err,data) {
+        if(err){
+            throw err;
+        }
+        var dataLength = 0;
+        for(var i=0; i<data.length; i++){
+            dataLength++
+        }
+        console.log(data);
+        req.session.searchResult = {searchContents:searchContents,searchResult:data,dataLength:dataLength};
+        res.send({code:1});
+    });
 
 
 
