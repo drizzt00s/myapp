cf40d57479b2a8ff2c10723db8f7a319c74f47cf
drizzt00s <drizzt79s@163.com>
2021-05-20 10:44:44 +0800

    d

---
+++ routes/admin_reply.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var utility = require("../public/javascripts/utility");
+var router = express.Router();
+
+router.post('/', function(req, res, next) {
+    var reply = req.body.reply;
+    var admin = req.body.admin;
+    var questionId = req.body.questionId;
+    var connection = utility.createConnection("localhost", "root", "YES", "3306", "app");
+    //var sql = "UPDATE ques (answ,ques_admin) VALUES(?,?) WHERE id=" + "'" + questionId + "'";
+    var sql = "UPDATE ques SET answ=" +"'" + reply +"'" + "," + "ques_admin=" +"'" + admin +"'" +"WHERE id="+"'" + questionId +"'";
+    utility.connect(connection);
+    connection.query(sql,function(err, result){
+        if(err){
+            throw err;
+        }
+        res.send({
+            code:1
+        });
+    })
+});
+
+module.exports = router;
