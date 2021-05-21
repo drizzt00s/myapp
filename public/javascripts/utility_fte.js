25a9f6c73872992a5bbdc420d682d943e4715acd
drizzt00s <drizzt79s@163.com>
2021-05-21 16:53:35 +0800

    fix header nav

--- public/javascripts/utility_fte.js
+++ public/javascripts/utility_fte.js
@@ -12,13 +12,49 @@ utility_fte.isSignin = function (resolve) {
             }
         }
     });
-}
+};
+
+
 utility_fte.get_pid = function () {
     var pid = "";
     var url = window.location.href;
     var pid = url.substring((url.indexOf("=") + 1));
     return pid;
-}
+};
+
+
+utility_fte.initHeaderBav = function () {
+    $(".navSelect").mouseover(function(e){
+        e.stopPropagation();
+        var index = $(e.target).attr("navSelect");
+        if(index == undefined){
+            return;
+        }
+        if(index == 1){
+            $.each($("dl.header_list_more_ul_main_all_con"), function(i, v){
+                if($(v).attr("parentID") == 1){
+                    $(v).removeClass("myHidden");
+                }
+            });
+        }
+    });
+
+    $("li.gpdListLi").mouseover(function(e){
+        e.stopPropagation();
+        var proID = $(e.target).find("span.gpdListTag").text();
+        if(proID == 0){
+            return;
+        }
+        $.each($("dl.header_list_more_ul_main_all_con"), function(i,v){
+            if($(v).attr("parentID") == proID){
+                $(v).removeClass("myHidden");
+            }else{
+                $(v).addClass("myHidden");
+            }
+        });
+        // console.log(proID);
+    });
+};
 
 
 
