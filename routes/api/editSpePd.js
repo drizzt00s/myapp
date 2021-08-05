df9cded4f9e928a41a7fdec07631c46e59926303
drizzt00s <drizzt79s@163.com>
2021-08-05 13:31:13 +0800

    d

--- routes/api/editSpePd.js
+++ routes/api/editSpePd.js
@@ -136,8 +136,7 @@ router.post("/", function(req, res, next){
                                 }
                                 
                                 console.log("ok");
-                                
-    
+                                res.redirect("/editProduct");
                             });
                             
     
